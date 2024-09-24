51bc2754eb7be777eb159427bda24cc2fcc5a687
fvlaenix <fvlaenixf@gmail.com>
2024-09-24 10:22:30 +0400

    Add oval image inserter

--- src/main/kotlin/utils/Text2ImageUtils.kt
+++ src/main/kotlin/utils/Text2ImageUtils.kt
@@ -1,6 +1,8 @@
 package utils
 
+import bean.BlockData
 import bean.BlockSettings
+import bean.BlockType
 import java.awt.BasicStroke
 import java.awt.FontMetrics
 import java.awt.Graphics2D
@@ -8,9 +10,11 @@ import java.awt.RenderingHints
 import java.awt.font.GlyphVector
 import java.awt.image.BufferedImage
 import kotlin.math.min
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 object Text2ImageUtils {
-  private fun splitToLines(text: String, textMetrics: FontMetrics, width: Int): List<String> {
+  private fun splitToLinesRectangle(text: String, textMetrics: FontMetrics, width: Int): List<String> {
     val lines = text.lines()
     return lines.flatMap { line ->
       val arr = line.split(" ".toRegex()).toTypedArray()
@@ -28,30 +32,103 @@ object Text2ImageUtils {
     }
   }
 
+  private fun splitToLinesOval(text: String, textMetrics: FontMetrics, width: Int, height: Int): List<String> {
+    val lines = text.lines()
+    val result = mutableListOf<String>()
+    val centerY = height / 2
+
+    var currentY = 0
+    lines.flatMap { line ->
+      val words = line.split(" ")
+      var nIndex = 0
+
+      while (nIndex < words.size && currentY < height) {
+        var line = words[nIndex++]
+        val adjustedWidth = (width * sqrt(1.0 - ((currentY - centerY).toDouble() / centerY).pow(2.0))).toInt()
+
+        while (nIndex < words.size && textMetrics.stringWidth(line + " " + words[nIndex]) < adjustedWidth) {
+          line += " " + words[nIndex]
+          nIndex++
+        }
+
+        result.add(line)
+        currentY += textMetrics.height
+      }
+
+      result
+    }
+
+    return result
+  }
+
+  private fun splitToLines(textMetrics: FontMetrics, blockData: BlockData): List<String> {
+    return when (blockData.blockType) {
+      is BlockType.Rectangle -> {
+        val width = blockData.blockType.width - blockData.settings!!.border * 2
+        splitToLinesRectangle(blockData.text, textMetrics, width)
+      }
+
+      is BlockType.Oval -> {
+        val width = blockData.blockType.width - blockData.settings!!.border * 2
+        val height = blockData.blockType.height - blockData.settings!!.border * 2
+        splitToLinesOval(blockData.text, textMetrics, width, height)
+      }
+    }
+  }
+
   private fun textToImage(
-    graphics2D: Graphics2D,
-    x: Int,
-    y: Int,
-    width: Int,
-    height: Int,
-    text: String,
-    settings: BlockSettings
-  ): Boolean {
+    globalSettings: BlockSettings,
+    blockData: BlockData
+  ): Text2ImageResult {
+
+    val x: Int
+    val y: Int
+    val width: Int
+    val height: Int
+    when (blockData.blockType) {
+      is BlockType.Rectangle -> {
+        x = blockData.blockType.x
+        y = blockData.blockType.y
+        width = blockData.blockType.width
+        height = blockData.blockType.height
+      }
+      is BlockType.Oval -> {
+        x = blockData.blockType.x
+        y = blockData.blockType.y
+        width = blockData.blockType.width
+        height = blockData.blockType.height
+      }
+    }
+    val settings = blockData.settings ?: globalSettings
+
+    val image = BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
+    val graphics2D = image.createGraphics()
+
     graphics2D.font = settings.font
     val fontMetrics = graphics2D.fontMetrics
     val textX = x + settings.border
     val textWidth = width - settings.border * 2
     val outlineStroke = BasicStroke(settings.outlineSize.toFloat())
 
-    val lines = splitToLines(text, fontMetrics, textWidth)
+    val lines = splitToLines(fontMetrics, blockData)
     val isOutOfBorder = lines.size * fontMetrics.height + settings.border > height
 
     // background
     val backgroundColor = settings.backgroundColor
     if (backgroundColor.a != 0) {
       graphics2D.color = backgroundColor.color
-      val height = min(lines.size * fontMetrics.height + settings.border * 2, height)
-      graphics2D.fillRect(x, y, width, height)
+
+      when (blockData.blockType) {
+        is BlockType.Rectangle -> {
+          val rectangleHeight = min(lines.size * fontMetrics.height + settings.border * 2, height)
+          graphics2D.fillRect(x, y, width, rectangleHeight)
+        }
+
+        is BlockType.Oval -> {
+          val ovalHeight = min(lines.size * fontMetrics.height + settings.border * 2, height)
+          graphics2D.fillOval(x, y, width, ovalHeight)
+        }
+      }
     }
 
     // text
@@ -83,7 +160,7 @@ object Text2ImageUtils {
       graphics2D.stroke = originalStroke
       graphics2D.setRenderingHints(originalHints)
     }
-    return isOutOfBorder
+    return Text2ImageResult(image, isOutOfBorder)
   }
 
   fun createSample(
@@ -92,10 +169,17 @@ object Text2ImageUtils {
     settings: BlockSettings
   ): BufferedImage {
     val text =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rhoncus iaculis orci, a interdum risus pellentesque et. Pellentesque fringilla massa"
-    val image = BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
-    val graphics2D = image.createGraphics()
-    textToImage(graphics2D, 0, 0, width, height, text, settings)
-    return image
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis quam tempor, tempus lorem id, aliquet ante. Ut vitae mi blandit, tincidunt arcu eu, mattis metus. Donec eget tincidunt quam, nec tempor ligula. Donec ornare mi nisl, quis imperdiet erat laoreet quis. Phasellus sed neque non sem consequat pharetra. Fusce ultrices erat nec tincidunt vehicula. Duis non odio pharetra, laoreet ipsum varius, porta libero."
+    val blockData = BlockData(
+      blockType = BlockType.Oval(0, 0, width, height),
+      text = text,
+      settings = settings
+    )
+    return textToImage(settings, blockData).image
   }
+
+  class Text2ImageResult(
+    val image: BufferedImage,
+    val isOutOfBorder: Boolean
+  )
 }
